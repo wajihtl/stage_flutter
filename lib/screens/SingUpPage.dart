5709042f00919d712878bfdafc8039ca2c9c7e00
wajihtl <wajihtlili@esprit.tn>
2021-08-04 17:08:21 +0100

    ontrole sasie signup le 04/08/2021

--- lib/screens/SingUpPage.dart
+++ lib/screens/SingUpPage.dart
@@ -2,10 +2,12 @@ import 'dart:io';
 import 'dart:ui';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:easyship/services/global_method.dart';
 import 'package:easyship/tabs/Sender/Profile_S.dart';
 import 'package:easyship/widgets/Sender/Navigation_S.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -24,8 +26,9 @@ class _SignUpPageState extends State<SignUpPage> {
   Color _stepper_color1 = Colors.grey;
   Color _stepper_color2 = Colors.grey;
 
-  String _dropdownValue = "Country";
   bool agree = false;
+  bool _obscureText1 = true;
+  bool _obscureText2 = true;
 
   File _image;
   List<File> _pickedIamages = [];
@@ -45,48 +48,77 @@ class _SignUpPageState extends State<SignUpPage> {
     });
   }
 
-  TextEditingController _emailControl = new TextEditingController();
-  TextEditingController _passwordControl = new TextEditingController();
+  String _dropdownValuee = "Country";
+
+  TextEditingController _FirstName = new TextEditingController();
+  TextEditingController _LastName = new TextEditingController();
+  TextEditingController _UserName = new TextEditingController();
+  TextEditingController _Email = new TextEditingController();
+  TextEditingController _Password = new TextEditingController();
+  TextEditingController _dropdownValue = new TextEditingController();
+  TextEditingController _PhoneNumber = new TextEditingController();
 
   // firebase connect
 
   bool _isLoading = false;
   String _errMessage = "";
+  String url;
+
+  /////////////////////////////////////
+  GlobalMethods _globalMethods = GlobalMethods();
+
+  final _formKey = GlobalKey<FormState>();
 
   FirebaseAuth auth = FirebaseAuth.instance;
   FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-  _signupAccount(String email, String password) {
-    setState(() {
-      _isLoading = !_isLoading;
-      _errMessage = "";
-    });
-    auth
-        .createUserWithEmailAndPassword(email: email, password: password)
-        .then((response) {
-      setState(() {
-        _isLoading = !_isLoading;
-      });
+  void _signupAccount() async {
+    final isValid = _formKey.currentState.validate();
+    if (isValid) {
+      _formKey.currentState.save();
+      try {
+        if (_image == null) {
+          _globalMethods.authErrorHandle('Please pick an image', context);
+        } else {
+          setState(() {
+            _isLoading = true;
+          });
 
-      print(response);
+          final ref = FirebaseStorage.instance
+              .ref()
+              .child('usersImages')
+              .child(_FirstName.text + '.jpg');
+          await ref.putFile(_image);
+          url = await ref.getDownloadURL();
+          await auth.createUserWithEmailAndPassword(
+              email: _Email.text.trim(), password: _Password.text.trim());
+          final User user = auth.currentUser;
+          final _uid = user.uid;
 
-      // insert user firestore
+          await FirebaseFirestore.instance.collection('users').doc(_uid).set({
+            'id': _uid,
+            'First name': _FirstName.text,
+            'Last name': _LastName.text,
+            'User name': _UserName.text,
+            'email': _Email.text,
+            'Phone Number': _PhoneNumber.text,
+            'Country': _dropdownValue.text,
+            'imageUrl': url,
+            'createdAt': Timestamp.now(),
+          });
 
-      firestore
-          .collection("users")
-          .doc(response.user.uid)
-          .set({"email": response.user.email, "Test": _emailControl.text}).then(
-              (value) => null);
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => Navigation_S()),
-      );
-    }).catchError((e) {
-      setState(() {
-        _isLoading = !_isLoading;
-        _errMessage = e.code;
-      });
-    });
+          //MaterialPageRoute(builder: (context) => Navigation_S()),
+          Navigator.canPop(context) ? Navigator.pop(context) : null;
+        }
+      } catch (error) {
+        _globalMethods.authErrorHandle(error.message, context);
+        print('error occurred ${error.message}');
+      } finally {
+        setState(() {
+          _isLoading = false;
+        });
+      }
+    }
   }
 
   @override
@@ -175,80 +207,131 @@ class _SignUpPageState extends State<SignUpPage> {
                           color: _stepper_color1,
                         ),
                         title: Text('Account'),
-                        content: Column(
-                          children: <Widget>[
-                            TextFormField(
-                              decoration:
-                                  InputDecoration(labelText: 'First Name'),
-                            ),
-                            TextFormField(
-                              decoration:
-                                  InputDecoration(labelText: 'Last Name'),
-                            ),
-                            TextFormField(
-                              decoration:
-                                  InputDecoration(labelText: 'Username'),
-                            ),
-                            TextFormField(
-                              controller: _emailControl,
-                              decoration: InputDecoration(labelText: 'E-mail'),
-                            ),
-                            TextFormField(
-                              decoration:
-                                  InputDecoration(labelText: 'Phone Number'),
-                            ),
-                            DropdownButton<String>(
-                              underline: Divider(
-                                color: Colors.white,
-                              ),
-                              isExpanded: true,
-                              hint: TextFormField(
+                        content: Form(
+                          key: _formKey,
+                          child: Column(
+                            children: <Widget>[
+                              TextFormField(
+                                key: ValueKey('FirestName'),
+                                validator: (value) {
+                                  if (value.isEmpty) {
+                                    return 'name cannot be null';
+                                  }
+                                  return null;
+                                },
+                                keyboardType: TextInputType.name,
+                                controller: _FirstName,
                                 decoration:
-                                    InputDecoration(labelText: _dropdownValue),
+                                    InputDecoration(labelText: 'First Name'),
                               ),
-                              items: <String>['France', 'Tunis']
-                                  .map((String value) {
-                                return DropdownMenuItem<String>(
-                                  value: value,
-                                  child: Text(value),
-                                );
-                              }).toList(),
-                              onChanged: (String newValue) {
-                                setState(() {
-                                  _dropdownValue = newValue;
-                                });
-                              },
-                            ),
-                            TextFormField(
-                              controller: _passwordControl,
-                              decoration:
-                                  InputDecoration(labelText: 'Password'),
-                            ),
-                            TextFormField(
-                              decoration: InputDecoration(
-                                  labelText: 'Confirm password'),
-                            ),
-                            Row(
-                              children: [
-                                Checkbox(
-                                  value: agree,
-                                  onChanged: (value) {
-                                    setState(() {
-                                      agree = value;
-                                    });
-                                  },
+                              TextFormField(
+                                key: ValueKey('LastName'),
+                                keyboardType: TextInputType.name,
+                                controller: _LastName,
+                                decoration:
+                                    InputDecoration(labelText: 'Last Name'),
+                              ),
+                              TextFormField(
+                                key: ValueKey('UserName'),
+                                keyboardType: TextInputType.name,
+                                controller: _UserName,
+                                decoration:
+                                    InputDecoration(labelText: 'Username'),
+                              ),
+                              TextFormField(
+                                key: ValueKey('Email'),
+                                keyboardType: TextInputType.emailAddress,
+                                controller: _Email,
+                                decoration: InputDecoration(labelText: 'E-mail'),
+                              ),
+                              TextFormField(
+                                key: ValueKey('PhoneNumber'),
+                                keyboardType: TextInputType.phone,
+                                controller: _PhoneNumber,
+                                decoration:
+                                    InputDecoration(labelText: 'Phone Number'),
+                              ),
+                              DropdownButton<String>(
+                                underline: Divider(
+                                  color: Colors.white,
                                 ),
-                                Container(
-                                  padding:
-                                      EdgeInsets.only(top: 30.0, bottom: 30.0),
-                                  width: 200.0,
-                                  child: Text(
-                                    'I have read and accept terms and conditions.',
+                                isExpanded: true,
+                                hint: TextFormField(
+                                  decoration:
+                                      InputDecoration(labelText: _dropdownValuee),
+                                ),
+                                items: <String>['France', 'Tunis']
+                                    .map((String country) {
+                                  return DropdownMenuItem<String>(
+                                    value: country,
+                                    child: Text(country),
+                                  );
+                                }).toList(),
+                                onChanged: (String newValue) {
+                                  setState(() {
+                                    _dropdownValue.text = newValue;
+                                  });
+                                  _dropdownValuee = newValue;
+                                },
+                              ),
+                              TextFormField(
+                                key: ValueKey('Password1'),
+                                obscureText: _obscureText1,
+                                keyboardType: TextInputType.visiblePassword,
+                                controller: _Password,
+                                decoration: InputDecoration(
+                                  labelText: 'Password',
+                                  suffixIcon: GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        _obscureText1 = !_obscureText1;
+                                      });
+                                    },
+                                    child: Icon(_obscureText1
+                                        ? Icons.visibility
+                                        : Icons.visibility_off),
                                   ),
                                 ),
-                              ],
-                            ),
-                          ],
+                              ),
+                              TextFormField(
+                                key: ValueKey('Password2'),
+                                obscureText: _obscureText2,
+                                keyboardType: TextInputType.visiblePassword,
+                                decoration: InputDecoration(
+                                    suffixIcon: GestureDetector(
+                                      onTap: () {
+                                        setState(() {
+                                          _obscureText2 = !_obscureText2;
+                                        });
+                                      },
+                                      child: Icon(_obscureText2
+                                          ? Icons.visibility
+                                          : Icons.visibility_off),
+                                    ),
+                                    labelText: 'Confirm password'),
+                              ),
+                              Row(
+                                children: [
+                                  Checkbox(
+                                    value: agree,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        agree = value;
+                                      });
+                                    },
+                                  ),
+                                  Container(
+                                    padding:
+                                        EdgeInsets.only(top: 30.0, bottom: 30.0),
+                                    width: 200.0,
+                                    child: Text(
+                                      'I have read and accept terms and conditions.',
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
                         isActive: _currentStep >= 0,
                         state: _currentStep == 0
@@ -309,6 +392,23 @@ class _SignUpPageState extends State<SignUpPage> {
                                     });
                               },
                             ),
+                            _isLoading
+                                ? Container(
+                                    child: Column(
+                                    children: [
+                                      Text("This may take a few seconds"),
+                                      SizedBox(
+                                        height: 10.0,
+                                      ),
+                                      CircularProgressIndicator(
+                                        backgroundColor: widget.Theme,
+                                      ),
+                                    ],
+                                  ))
+                                : Text("Add a photo"),
+                            SizedBox(
+                              height: 20.0,
+                            )
                           ],
                         ),
                         isActive: _currentStep >= 1,
@@ -343,11 +443,10 @@ class _SignUpPageState extends State<SignUpPage> {
   }
 
   continued() {
-    _currentStep == 1
-        ? _signupAccount(_emailControl.text, _passwordControl.text)
-        : null;
+    _currentStep == 1 ? _signupAccount() : null;
 
     _currentStep < 1 ? setState(() => _currentStep += 1) : null;
+
 //coloring icons
     switch (_currentStep) {
       case 0:
--- lib/screens/scnd_screen.dart
+++ lib/screens/scnd_screen.dart
@@ -7,84 +7,87 @@ class scnd_screen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.height,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/images/easyship.png'),
-                fit: BoxFit.fill,
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/easyship.png'),
+                  fit: BoxFit.fill,
+                ),
               ),
             ),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "WELCOME",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 55,
-                    fontWeight: FontWeight.bold),
-              ),
-              Text(
-                "TO",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 55,
-                    fontWeight: FontWeight.bold),
-              ),
-              Padding(
-                padding: EdgeInsets.only(
-                    top: MediaQuery.of(context).size.height * 0.4),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    RaisedButton(
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "WELCOME",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 55,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "TO",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 55,
+                      fontWeight: FontWeight.bold),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.4),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      RaisedButton(
                         elevation: 10.0,
-                        child:  Padding(
-                          padding:  EdgeInsets.symmetric(horizontal: 60.0, vertical: 10.0),
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 60.0, vertical: 10.0),
                           child: Text('I\'m a Traveler'),
                         ),
                         textColor: Colors.white,
-                        shape:  RoundedRectangleBorder(
-                          borderRadius:  BorderRadius.circular(30.0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0),
                         ),
                         color: Colors.purpleAccent[700],
                         onPressed: () => Navigator.push(
                             context,
-                            MaterialPageRoute(builder: (context) => Navigation_T())),
-
-                    ),
+                            MaterialPageRoute(
+                                builder: (context) => Navigation_T())),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    RaisedButton(
+                        elevation: 10.0,
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 60.0, vertical: 10.0),
+                          child: Text('I\'m a Sender   '),
+                        ),
+                        textColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                        color: Colors.cyan[700],
+                        onPressed: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Navigation_S()))),
                   ],
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                   RaisedButton(
-                     elevation: 10.0,
-                    child: Padding(
-                      padding:  EdgeInsets.symmetric(horizontal: 60.0, vertical: 10.0),
-                      child:  Text('I\'m a Sender   '),
-                    ),
-                    textColor: Colors.white,
-                    shape:  RoundedRectangleBorder(
-                      borderRadius:  BorderRadius.circular(30.0),
-                    ),
-                    color: Colors.cyan[700],
-                    onPressed: () => Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => Navigation_S()))
-
-                  ),
-                ],
-              ),
-            ],
-          )
-        ],
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
